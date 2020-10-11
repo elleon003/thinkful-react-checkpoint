66b0d4a830e070b0d27d4153b9a8dc6472513914
elleon003 <ngrandison@ymail.com>
2020-10-11 17:36:48 -0400

    Checkpoint 11 Callback props

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './Card.css';
 
-class Card extends React.Component{
-  render() {
+export default function Card(props){
     return(
       <div className='Card'>
-        <button type='button'>
+        <button 
+        type='button'
+        onClick={() => props.onClickDelete(props.id)}
+        >
           delete
         </button>
-        <h3>{this.props.title}</h3>
-        <p>{this.props.content}</p>
+        <h3>{props.title}</h3>
+        <p>{props.content}</p>
       </div>
     )
   }
-}
+  Card.propTypes = {
+    onClickDelete: () => {}
+  }
 
-export default Card;
--- src/List.js
+++ src/List.js
@@ -3,28 +3,33 @@ import Card from './Card';
 import './List.css';
 
 
-class List extends React.Component{
-  render() {
+export default function List(props) {
     return(
       <section className='List'>
         <header className='List-header'>
-          <h2>{this.props.header}</h2>
+          <h2>{props.header}</h2>
         </header>
         <div className='List-cards'>
-          {this.props.cards.map((card) =>
+          {props.cards.map((card) =>
             <Card 
               key={card.id}
+              id={card.id}
               title={card.title}
               content={card.content}
+              onClickDelete={props.onClickDelete}
             />
           )}
-          <button type="button" className="List-add-button">
+          <button 
+          type="button" className="List-add-button"
+          onClick={() => props.onClickAdd(props.id)}
+          >
             + Add Random Card
           </button>
         </div>
       </section>
     )
   }
-}
 
-export default List;
+  List.defaultProps = {
+    onClickAdd: () => {}
+  }
